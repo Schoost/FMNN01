2a59e864ab9c6baba2b01c6052fe1ce961b662e9
Joost Kranenborg <joost.kranenborg@gmail.com>
2017-10-31 11:11:49 +0100

    fixed norms

--- eigenvaluesIteration.py
+++ eigenvaluesIteration.py
@@ -2,18 +2,26 @@ import numpy as np
 import scipy.linalg as sl
 
 
-def powerIter(A,tol=0.00001,maxIter=100000):
+def powerIter(A,tol=0.00001,maxIter=1000):
     sh = np.shape(A)
     v=np.random.rand(sh[0],1)
     for i in range(0,maxIter):
         vOld = v
         v=A@v
-        v=v/(sqrt(v.T@v))
-        if (sl.norm(vOld-v)<tol):
+        v=v/sl.norm(v)
+        if (sl.norm(vOld-v)<tol or sl.norm(vOld+v)<tol):
             break
     return v,i
 
-
-
+def inverseIter(A,mu=1,tol=0.00001,maxIter=1000):
+    sh = np.shape(A)
+    v=np.random.rand(sh[0],1)
+    for i in range(0,maxIter):
+        vOld = v
+        v=sl.solve(A-mu*np.identity(sh[0]),v)
+        v=v/sl.norm(v)
+        if (sl.norm(vOld-v)<tol  or sl.norm(vOld+v)<tol):
+            break
+    return v,i
 def rayleigh(A,x):
     return x.T@A@x/(x.T@x)
